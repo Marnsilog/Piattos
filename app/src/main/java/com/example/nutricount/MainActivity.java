0683021798018dd5c5a1ea9195d96b108a5a1aba
Marniel Corsiga <corsiga.242722@globalcity.sti.edu.ph>
2024-06-04 14:44:19 +0800

    fdas

--- app/src/main/java/com/example/nutricount/MainActivity.java
+++ app/src/main/java/com/example/nutricount/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.nutricount;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,17 +13,61 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+ProgressBar progressBar;
+private Handler handler = new Handler();
+private int progressStatus = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        progressBar = (ProgressBar) findViewById(R.id.pb1);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Intent intent  = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 4;
+                    //Update progress bar with completion of operation
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+                    try {
+                        // Sleep for 300 milliseconds.
+                        //Just to display the progress slowly
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        class MyProgressBar extends ProgressBar
+        {
+            public MyProgressBar(Context context) {
+                super(context);
+            }
+
+            @Override
+            public void setProgress(int progress)
+            {
+                super.setProgress(progress);
+                if(progress == this.getMax())
+                {
+                    Intent intent  = new Intent(this.getContext(), MainActivity2.class);
+                    startActivity(intent);
+                }
+            }
+        }
     }
+
     //test
 }
--- app/src/main/java/com/example/nutricount/MainActivity2.java
+++ app/src/main/java/com/example/nutricount/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.example.nutricount;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity2 extends AppCompatActivity {
 
+    EditText username, email, pass, confirmpass;
+    Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        pass = findViewById(R.id.pass);
+        confirmpass = findViewById(R.id.confrm);
+        btnSubmit = findViewById(R.id.btnsignup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    if(username==null || email==null ||pass==null || confirmpass==null) {
+                        Toast.makeText(MainActivity2.this, "INVALID ACCOUNT", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(pass!=confirmpass) {
+                        Toast.makeText(MainActivity2.this, "PASSWORD AND CONFIRM PASSWORD IS INVALID", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        String u = username.toString();
+                        String e = email.toString();
+                        String p = pass.toString();
+                        dbConnection db = new dbConnection(MainActivity2.this);
+                        db.addUser(u,e,p);
+                    }
+
+
+
+            }
+        });
     }
+
+
+
 }
